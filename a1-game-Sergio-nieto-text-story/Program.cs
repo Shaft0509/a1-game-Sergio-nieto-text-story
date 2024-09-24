0692e7a89f92f50a37379dd50fa8307919d42c83
Shaft0509 <sergionieto555@gmail.com>
2024-09-24 17:19:56 -0500

    Add choices

--- a1-game-Sergio-nieto-text-story/Program.cs
+++ a1-game-Sergio-nieto-text-story/Program.cs
@@ -17,3 +17,18 @@ no = "caspian spared his life";
 Console.WriteLine("Caspian enters the devil's cage and defeats the world´s strongest devil Demogorgon");
 Console.WriteLine("Do you want Caspian to kill the devil?");
 
+string input = Console.ReadLine();
+
+if (input.ToUpper() == "YES")
+{
+    Console.WriteLine("Caspian killed the devil and became the world´s saviour, however that left him a bitter flavour");
+}
+else if (input.ToUpper() == "NO")
+{
+    Console.WriteLine("Caspian spared his life");
+    Console.WriteLine("Should Caspian talk to the devil about human kindness");
+}
+
+
+
+
