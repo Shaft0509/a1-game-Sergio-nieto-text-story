9b02632e56ed8e895d825fec21a5e2153406047c
Shaft0509 <sergionieto555@gmail.com>
2024-09-24 18:29:23 -0500

    Final

--- a1-game-Sergio-nieto-text-story/Program.cs
+++ a1-game-Sergio-nieto-text-story/Program.cs
@@ -17,12 +17,15 @@ no = "caspian spared his life";
 Console.WriteLine("Caspian enters the devil's cage and defeats the world´s strongest devil Demogorgon");
 Console.WriteLine("Do you want Caspian to kill the devil?");
 
+// Choices
+
 string input = Console.ReadLine();
 
 if (input.ToUpper() == "YES")
 {
     Console.WriteLine("Caspian killed the devil and became the world´s saviour, however that left him a bitter flavour");
     Console.WriteLine("Neutral ending");
+    Console.WriteLine("Thank you for playing!, you should try the other endings!");
 }
 else if (input.ToUpper() == "NO")
 {
@@ -35,15 +38,19 @@ else if (input.ToUpper() == "NO")
     {
         Console.WriteLine("The devil understands human kindness and begins a life of good deeds and spends the rest of his life redeeming his sins");
         Console.WriteLine("Good ending :)");
+        Console.WriteLine("This game leaves you the moral of communication solves problems.");
   }
 
     else if (input2.ToUpper() == "NO")
     {
         Console.WriteLine("The devil takes advantages of Caspian´s naiveness and kills him from the back");
         Console.WriteLine("Bad ending...");
+        Console.WriteLine("Demogorgon took over the world... Try other endings!");
     }
 }
 
+
+// Note: I know it is very simple, but I am trying to get better.
 
 
 
