205db068a845edafd6eac7fc6fb3102f2c1f59d9
Shaft0509 <sergionieto555@gmail.com>
2024-09-24 18:24:49 -0500

    Added the statements for the "NO" choice and added some details.

--- a1-game-Sergio-nieto-text-story/Program.cs
+++ a1-game-Sergio-nieto-text-story/Program.cs
@@ -22,11 +22,26 @@ string input = Console.ReadLine();
 if (input.ToUpper() == "YES")
 {
     Console.WriteLine("Caspian killed the devil and became the world´s saviour, however that left him a bitter flavour");
+    Console.WriteLine("Neutral ending");
 }
 else if (input.ToUpper() == "NO")
 {
     Console.WriteLine("Caspian spared his life");
-    Console.WriteLine("Should Caspian talk to the devil about human kindness");
+    Console.WriteLine("Should Caspian talk to the devil about human kindness?");
+
+    string input2 = Console.ReadLine();
+
+    if (input2.ToUpper() == "YES")
+    {
+        Console.WriteLine("The devil understands human kindness and begins a life of good deeds and spends the rest of his life redeeming his sins");
+        Console.WriteLine("Good ending :)");
+  }
+
+    else if (input2.ToUpper() == "NO")
+    {
+        Console.WriteLine("The devil takes advantages of Caspian´s naiveness and kills him from the back");
+        Console.WriteLine("Bad ending...");
+    }
 }
 
 
